1b46d2b2a55f0bb52b2fcbca3a3259501eb08ecd
markdalgleish <mark.john.dalgleish@gmail.com>
2022-07-07 21:20:41 +1000

    Move selection from text node to parent frame after trimming

--- src/main.ts
+++ src/main.ts
@@ -28,92 +28,96 @@ export default async function () {
     )
   );
 
-  figma.currentPage.selection.forEach((selectedNode) => {
-    const textNode =
-      selectedNode.type === "TEXT"
-        ? selectedNode
-        : selectedNode.type === "FRAME" &&
-          isNodeOwned(selectedNode) &&
-          selectedNode.children.length === 1 &&
-          selectedNode.children[0].type === "TEXT"
-        ? selectedNode.children[0]
-        : null;
+  figma.currentPage.selection = figma.currentPage.selection.map(
+    (selectedNode): SceneNode => {
+      const textNode =
+        selectedNode.type === "TEXT"
+          ? selectedNode
+          : selectedNode.type === "FRAME" &&
+            isNodeOwned(selectedNode) &&
+            selectedNode.children.length === 1 &&
+            selectedNode.children[0].type === "TEXT"
+          ? selectedNode.children[0]
+          : null;
 
-    if (!textNode) {
-      return;
+      if (!textNode) {
+        return selectedNode;
+      }
+
+      if (textNode.fontName === figma.mixed) {
+        figma.notify("Leading cannot be trimmed from text with mixed fonts.");
+        return selectedNode;
+      }
+
+      if (textNode.fontSize === figma.mixed) {
+        figma.notify("Leading cannot be trimmed from text with mixed sizes.");
+        return selectedNode;
+      }
+
+      if (textNode.lineHeight === figma.mixed) {
+        figma.notify(
+          "Leading cannot be trimmed from text with mixed line heights."
+        );
+        return selectedNode;
+      }
+
+      if (!(textNode.fontName.family in fontMetrics)) {
+        figma.notify(
+          `The font "${textNode.fontName.family}" is not currently supported.`
+        );
+        return selectedNode;
+      }
+
+      if (textNode.textAutoResize === "NONE") {
+        textNode.textAutoResize = "HEIGHT";
+      }
+
+      const options = {
+        fontSize: textNode.fontSize,
+        leading:
+          textNode.lineHeight.unit !== "AUTO"
+            ? textNode.lineHeight.unit === "PERCENT"
+              ? textNode.fontSize * (textNode.lineHeight.value / 100)
+              : textNode.lineHeight.value
+            : undefined,
+        fontMetrics: fontMetrics[textNode.fontName.family],
+      };
+
+      const capsizeValues = precomputeValues(options);
+      const marginTop =
+        parseFloat(capsizeValues.capHeightTrim) * textNode.fontSize;
+      const marginBottom =
+        parseFloat(capsizeValues.baselineTrim) * textNode.fontSize;
+
+      // Add new margins
+      const parent = textNode.parent ?? figma.currentPage;
+      const index = parent.children.indexOf(textNode);
+
+      const isFirstRun = parent.type !== "FRAME" || !isNodeOwned(parent);
+      const frame = isFirstRun ? figma.createFrame() : (parent as FrameNode);
+      markNodeAsOwned(frame);
+
+      frame.name = " "; // This ensures a frame name is not visible in the UI
+      frame.fills = [];
+      frame.clipsContent = false; // Allows ascenders/descenders to be visible
+      frame.resize(textNode.width, textNode.height + marginTop + marginBottom);
+
+      if (isFirstRun) {
+        frame.appendChild(textNode);
+        frame.x = textNode.x;
+        frame.y = textNode.y - marginTop;
+        parent.insertChild(index, frame);
+      } else {
+        frame.x = frame.x + textNode.x;
+        frame.y = frame.y + textNode.y - marginTop;
+      }
+
+      textNode.x = 0;
+      textNode.y = marginTop;
+
+      return frame;
     }
-
-    if (textNode.fontName === figma.mixed) {
-      figma.notify("Leading cannot be trimmed from text with mixed fonts.");
-      return;
-    }
-
-    if (textNode.fontSize === figma.mixed) {
-      figma.notify("Leading cannot be trimmed from text with mixed sizes.");
-      return;
-    }
-
-    if (textNode.lineHeight === figma.mixed) {
-      figma.notify(
-        "Leading cannot be trimmed from text with mixed line heights."
-      );
-      return;
-    }
-
-    if (!(textNode.fontName.family in fontMetrics)) {
-      figma.notify(
-        `The font "${textNode.fontName.family}" is not currently supported.`
-      );
-      return;
-    }
-
-    if (textNode.textAutoResize === "NONE") {
-      textNode.textAutoResize = "HEIGHT";
-    }
-
-    const options = {
-      fontSize: textNode.fontSize,
-      leading:
-        textNode.lineHeight.unit !== "AUTO"
-          ? textNode.lineHeight.unit === "PERCENT"
-            ? textNode.fontSize * (textNode.lineHeight.value / 100)
-            : textNode.lineHeight.value
-          : undefined,
-      fontMetrics: fontMetrics[textNode.fontName.family],
-    };
-
-    const capsizeValues = precomputeValues(options);
-    const marginTop =
-      parseFloat(capsizeValues.capHeightTrim) * textNode.fontSize;
-    const marginBottom =
-      parseFloat(capsizeValues.baselineTrim) * textNode.fontSize;
-
-    // Add new margins
-    const parent = textNode.parent ?? figma.currentPage;
-    const index = parent.children.indexOf(textNode);
-
-    const isFirstRun = parent.type !== "FRAME" || !isNodeOwned(parent);
-    const frame = isFirstRun ? figma.createFrame() : (parent as FrameNode);
-    markNodeAsOwned(frame);
-
-    frame.name = " "; // This ensures a frame name is not visible in the UI
-    frame.fills = [];
-    frame.clipsContent = false; // Allows ascenders/descenders to be visible
-    frame.resize(textNode.width, textNode.height + marginTop + marginBottom);
-
-    if (isFirstRun) {
-      frame.appendChild(textNode);
-      frame.x = textNode.x;
-      frame.y = textNode.y - marginTop;
-      parent.insertChild(index, frame);
-    } else {
-      frame.x = frame.x + textNode.x;
-      frame.y = frame.y + textNode.y - marginTop;
-    }
-
-    textNode.x = 0;
-    textNode.y = marginTop;
-  });
+  );
 
   figma.closePlugin();
 }
